202af22c9422974a4c9c0dd52d760d099075be1b
aruyamam <aruyamam@gmail.com>
2019-08-19 15:49:20 +0900

    Testing MethodValidator

--- src/Helper/Route/Validation/Type/ControllerValidator.php
+++ src/Helper/Route/Validation/Type/ControllerValidator.php
@@ -4,6 +4,8 @@ namespace App\Helper\Route\Validation\Type;
 
 use App\Helper\Route\Validation\AbstractType;
 use App\Helper\Route\Validation\InterfaceValidation;
+use ReflectionClass;
+use ReflectionException;
 
 class ControllerValidator extends AbstractType implements InterfaceValidation
 {
@@ -11,5 +13,30 @@ class ControllerValidator extends AbstractType implements InterfaceValidation
     * @return boolean
     */
    public function isValid(): bool
-   { }
+   {
+      $isValid = false;
+      $className = $this->route->getController();
+
+      if (false === empty($className)) {
+         $isValid = $this->doesExist($className);
+      }
+
+      return $isValid;
+   }
+
+   /**
+    * @param string $string
+    *
+    * @return boolean
+    */
+   public function doesExist(string $string): bool
+   {
+      $isValid = true;
+      try {
+         new ReflectionClass($string);
+      } catch (ReflectionException $exception) {
+         $isValid = false;
+      }
+      return $isValid;
+   }
 }
